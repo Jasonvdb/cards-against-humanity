1a170ef2cf3f98683959cc7386d614bfe763a2de
Jason van den Berg <jayvdb1@gmail.com>
2016-12-12 22:40:35 +0200

    Added text input control

---
+++ src/client/components/chat.js
@@ -0,0 +1,10 @@
+import "./chat.scss";
+
+import React, {Component} from "react";
+import {TextInput} from "./controls";
+
+export default class Chat extends Component {
+	render() {
+		return <TextInput />; //className="top-border"
+	}
+}
